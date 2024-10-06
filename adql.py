b2623a187e64a80b89b4db709e353823e23d364f
EstherRamC <esther.ramced@gmail.com>
2024-10-06 15:21:27 +1100

    Query for one planet

--- adql.py
+++ adql.py
@@ -2,10 +2,20 @@
 from astroquery.gaia import Gaia
 import pandas as pd
 
+# Coordinates for the planet
+
+planet_ra = 270.0
+planet_dec = 66.56070833
+
 # ADQL query to get the data
 query = """
-SELECT source_id, ra, dec, parallax, phot_g_mean_mag 
+SELECT source_id, ra, dec, parallax, phot_g_mean_mag, 
+       DISTANCE(POINT({planet_ra}, {planet_dec}), POINT(ra, dec)) AS angular_distance
 FROM gaiadr3.gaia_source
+WHERE 1=CONTAINS(
+    POINT({planet_ra}, {planet_dec}), 
+    CIRCLE(POINT(ra, dec), 0.5)
+)
 """
 job = Gaia.launch_job(query)
 results = job.get_results()
